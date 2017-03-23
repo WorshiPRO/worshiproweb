7a3dae2e7113854cc48a8e877c4e116f1bf6a2c8
pingchihshih <b03902039@ntu.edu.tw>
2017-03-23 20:40:04 +0800

    mod: change join_us form action into function in check.js

--- assets/js/check.js
+++ assets/js/check.js
@@ -1,18 +1,19 @@
-/* var check = function(isHuman){
-    if(!isHuman){
-        alert("You are ROBOT!!!");
+var link = "https://formspree.io/worshipro@gmail.com";
+function submitform(){
+    if(!/\S/.test(document.getElementById('name').value)){
+        alert("請輸入名字"); 
+    }else if(!/\S/.test(document.getElementById('e_replytol').value)){
+        alert("請輸入電子信箱"); 
+    }else if(!/\S/.test(document.getElementById('message').value)){
+        alert("請輸入內容"); 
+    }else if(!document.getElementById('demo-human').checked){
+        alert("請勾選不是機器人");
     }else{
-        var form = document.createElement("form");
-        form.setAttribute("method", "post");
-        form.setAttribute("action", "https://formspree.io/worshipro@gmail.com");
-        document.body.appendChild(form);
-        form.submit();
-    }
-};*/
-function submitform()
-{
-    //if(document.getElementById('name').value = ""){
-       alert("請輸入名字"); 
-   // }else  {document.forms["my form"].submit();}
-        
+      //document.forms["my form"].submit();
+      var form = document.getElementById("my form");
+      form.setAttribute("method", "post");
+      form.setAttribute("action", link);
+      //document.body.appendChild(form);
+      form.submit();
+    }        
 };
